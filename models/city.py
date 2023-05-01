8d201c73afcbd3eebe006471d33a261d9193eb06
lizzyredd25 <oluwaseunoladele25@gmail.com>
2023-05-01 09:31:30 +0000

    models

---
+++ models/city.py
@@ -0,0 +1,14 @@
+ity class, a subclass of BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    A subclass of BaseModel class
+    Public class attributes:
+        state_id: (str) will be State.id
+        name:     (str)
+    """
+    state_id = ""
+    name = ""
